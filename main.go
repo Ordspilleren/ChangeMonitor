f3a4feff28f725f843aa0d57612221cab53b4b70
Lasse Bjerre <lasse@lgb.dk>
2021-05-13 03:00:49 +0200

    More changes, HTTP and cache works

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 	"sync"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var wg = &sync.WaitGroup{}
@@ -16,9 +24,12 @@ type Config struct {
 
 type Monitor struct {
 	URL         string        `json:"url"`
+	UseChrome   bool          `json:"useChrome"`
 	CSSSelector string        `json:"cssSelector"`
 	Interval    time.Duration `json:"interval"`
 	doneChannel chan bool
+	ticker      *time.Ticker
+	id          string
 }
 
 var config Config
@@ -45,9 +56,10 @@ func main() {
 }
 
 func StartMonitoring(monitors []Monitor) (err error) {
-	for _, monitor := range monitors {
-		monitor.doneChannel = make(chan bool)
-		monitorTicker := time.NewTicker(monitor.Interval * time.Second)
+	for idx := range monitors {
+		monitors[idx].id = generateSHA1String(monitors[idx].URL)
+		monitors[idx].doneChannel = make(chan bool)
+		monitors[idx].ticker = time.NewTicker(monitors[idx].Interval * time.Second)
 		wg.Add(1)
 		go func(monitor Monitor) {
 			for {
@@ -55,19 +67,96 @@ func StartMonitoring(monitors []Monitor) (err error) {
 				case <-monitor.doneChannel:
 					wg.Done()
 					return
-				case <-monitorTicker.C:
+				case <-monitor.ticker.C:
 					err = monitor.check()
 					if err != nil {
 						monitor.doneChannel <- true
 					}
 				}
 			}
-		}(monitor)
+		}(monitors[idx])
 	}
 	return
 }
 
+func generateSHA1String(input string) string {
+	h := sha1.New()
+	h.Write([]byte(input))
+	bs := h.Sum(nil)
+	return fmt.Sprintf("%x\n", bs)
+}
+
 func (m *Monitor) check() (err error) {
 	log.Print(m.URL)
+
+	if m.UseChrome {
+		log.Print("Using Chrome backend")
+	} else {
+		var headers http.Header
+		responseBody := getHTTPBody(m.URL, headers)
+		selectorContent := getCSSSelectorContent(responseBody, m.CSSSelector)
+		compareContent(m.id, selectorContent)
+	}
+
 	return
+}
+
+func getHTTPBody(url string, headers http.Header) io.ReadCloser {
+	httpClient := http.Client{}
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.Header = headers
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//defer response.Body.Close()
+
+	return response.Body
+}
+
+func getCSSSelectorContent(body io.ReadCloser, selector string) string {
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	selectorText := doc.Find(selector).First().Text()
+
+	return selectorText
+}
+
+func compareContent(monitorId string, content string) {
+	dataDir := "data"
+	filePath := filepath.Join(dataDir, monitorId)
+
+	os.Mkdir(dataDir, os.ModePerm)
+
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err := ioutil.WriteFile(filePath, []byte(content), 0644)
+			if err != nil {
+				log.Panic(err)
+			}
+			log.Print("File did not exist, creating and doing nothing...")
+			return
+		}
+	}
+
+	if string(fileData) == content {
+		log.Print("No changes.")
+		return
+	} else {
+		log.Print("Content has changed!")
+		err := ioutil.WriteFile(filePath, []byte(content), 0644)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 }
